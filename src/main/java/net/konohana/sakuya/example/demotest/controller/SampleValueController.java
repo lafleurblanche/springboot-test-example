e5c2f7cb3ffaccb86226dfd6d287f94eceaf1b26
lafleurblanche <aquamint@squall.dti.ne.jp>
2024-01-18 14:16:43 +0900

    20240118 DEBUG用処理追加

--- src/main/java/net/konohana/sakuya/example/demotest/controller/SampleValueController.java
+++ src/main/java/net/konohana/sakuya/example/demotest/controller/SampleValueController.java
@@ -1,5 +1,6 @@
 package net.konohana.sakuya.example.demotest.controller;
 
+import java.util.function.Consumer;
 import net.konohana.sakuya.example.demotest.utils.SampleJvmField;
 import net.konohana.sakuya.example.demotest.utils.SampleUtilsKt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,24 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleValueController {
+    Consumer<String> consumer = System.out::println;
+
     @GetMapping("/sample-value/java-string")
     public String getString() {
+        consumer.accept("Call: getString");
         return SampleUtilsKt.getStringValue();
     }
 
     @GetMapping("/sample-value/java-int")
     public int getInt() {
+        consumer.accept("Call: getInt");
         return SampleUtilsKt.getIntegerValue();
     }
 
     @GetMapping("/sample-value/jvm-field")
     public int getJvmField() {
+        consumer.accept("Call: getJvmField");
         SampleJvmField sampleJvmField = new SampleJvmField();
         return sampleJvmField.foo;
     }
 
     @GetMapping("/sample-value/non-jvm-field")
     public int getNonJvmField() {
+        consumer.accept("Call: getNonJvmField");
         SampleJvmField sampleJvmField = new SampleJvmField();
         return sampleJvmField.getBar();
     }
